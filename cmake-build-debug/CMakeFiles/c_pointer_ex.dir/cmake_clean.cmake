f14816b17d876210a425f830892421aa637589cf
shigemunek <katsuhiro.shigemune@gmail.com>
2021-05-01 16:24:16 +0900

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/c_pointer_ex.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/c_pointer_ex.dir/main.c.obj"
+  "c_pointer_ex.exe"
+  "c_pointer_ex.exe.manifest"
+  "c_pointer_ex.lib"
+  "c_pointer_ex.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/c_pointer_ex.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
